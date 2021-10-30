a50ca9caec0ea22f61c76b928a6fa29d5a96d2c5
yoga-saputra <0yogasaputra@gmail.com>
2021-10-30 14:03:01 +0700

    jwt nodejs

---
+++ index.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'bwamicro!123'
+
+//1. create basic token dengan proses syncronnus
+// const token = jwt.sign({ 
+//   data: {
+//     kelas: 'bwamicro'
+//   }
+// }, JWT_SECRET, { expiresIn: '5m' })
+// console.log(token);
+
+// 2. asyncronous create token
+// jwt.sign({
+//   data: {
+//     kelas: 'bwamicro'
+//   }
+// }, JWT_SECRET, {expiresIn: '1m'}, (err, token) => {
+//   console.log(token);
+// })
+
+// verify token cara 1
+
+const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImtlbGFzIjoiYndhbWljcm8ifSwiaWF0IjoxNjMyOTA2OTIyLCJleHAiOjE2MzI5MDcyMjJ9.7xHdiI_wvXGiLlAAXXIienESL3CVk6Gan12jJGflowk';
+// jwt.verify(token1, JWT_SECRET, (err, decoded) => {
+//   if (err) {
+//     console.log(err.message);
+//     return
+//   }
+//   console.log(decoded);
+// })
+
+// cara 2 verify
+
+try {
+  const decoded = jwt.verify(token1, JWT_SECRET)
+  console.log(decoded);
+} catch (error) {
+  console.log(error.message);
+}
